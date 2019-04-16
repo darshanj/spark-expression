5b45353f16ae238b064d57c0248b6f7548c92bbe
Darshan Jani <darshanj@thoughtworks.com>
2019-04-16 11:04:16 +0530

    Added array generation class and UDT

--- src/main/scala/org/apache/spark/sql/scala.scala
+++ src/main/scala/org/apache/spark/sql/scala.scala
@@ -1,7 +1,14 @@
 package org.apache.spark.sql
 
-import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.expressions.{Expression, GenArrayData}
+import org.apache.spark.sql.types.UserDefinedType
 
 object extensions {
   def withExpr(expr: Expression): Column = Column(expr)
+  object GenArrayDataCodeGen {
+    val genCodeToCreateArrayData = GenArrayData.genCodeToCreateArrayData _
+  }
+}
+object UserDefinedTypes {
+  abstract class UserDefinedTypesPublic[UserType >: Null] extends UserDefinedType[UserType]
 }
